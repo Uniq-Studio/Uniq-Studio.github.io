c0f53ec5e94417471e0597c4ccc1f1496ab888ab
Aaron <aaronleon28@gmail.com>
2025-12-17 20:52:43 +0000

    Created Smart Hero Banner

---
+++ javascript/hero.js
@@ -0,0 +1,16 @@
+function autoHero() {
+    setTimeout(changeHero, 2000)
+    setTimeout(removeAside, 2600)
+}
+
+function changeHero(){
+    document.getElementById('image_aside').style.opacity = "0";
+    document.getElementById('text_aside').style.opacity = "0";
+    document.getElementById('hero_section').style.opacity = "1";
+    document.getElementById('title_banner').style.height = "700px";
+}
+
+function removeAside(){
+    document.getElementById('image_aside').remove()
+    document.getElementById('text_aside').remove()
+}
